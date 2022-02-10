3f72b9d4d438b90b7189d40f21601d073c95ae2a
sj185226 <sj185226@ncr.com>
2022-02-10 16:20:03 +0530

    created model classes

--- src/main/java/com/example/jaspreet/demo/model/DataMatrix.java
+++ src/main/java/com/example/jaspreet/demo/model/DataMatrix.java
@@ -2,4 +2,31 @@ package com.example.jaspreet.demo.model;
 
 public class DataMatrix {
 
+    private double[][] distanceMatrix;
+    private double[][] timeMatrix;
+
+    public DataMatrix() {
+    }
+
+    public DataMatrix(double[][] distanceMatrix, double[][] timeMatrix) {
+        this.distanceMatrix = distanceMatrix;
+        this.timeMatrix = timeMatrix;
+    }
+
+    public double[][] getDistanceMatrix() {
+        return distanceMatrix;
+    }
+
+    public void setDistanceMatrix(double[][] distanceMatrix) {
+        this.distanceMatrix = distanceMatrix;
+    }
+
+    public double[][] getTimeMatrix() {
+        return timeMatrix;
+    }
+
+    public void setTimeMatrix(double[][] timeMatrix) {
+        this.timeMatrix = timeMatrix;
+    }
+
 }
