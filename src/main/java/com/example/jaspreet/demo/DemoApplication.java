fd74797b84ae5d874f92dece49887c01ac1895e4
Daryll Rebeiro <dr185212@ncr.com>
2022-02-11 13:21:43 +0530

    changed main class

--- src/main/java/com/example/jaspreet/demo/DemoApplication.java
+++ src/main/java/com/example/jaspreet/demo/DemoApplication.java
@@ -1,14 +1,34 @@
 package com.example.jaspreet.demo;
 
+import com.example.jaspreet.demo.model.Cashpoint;
+import com.example.jaspreet.demo.model.Parameters;
+import com.example.jaspreet.demo.service.DataParser;
+import com.example.jaspreet.demo.service.VRPSolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+
+	@Autowired
+	static DataParser dataParser;
+
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("hey.....");
+
+
+
+		List<Cashpoint> cashpoints = dataParser.getData();
+		Parameters param = dataParser.getParameters();
+		VRPSolver vrpsolver = new VRPSolver();
+		vrpsolver.solve(cashpoints, param, dataMatrix);
 	}
 
 }
